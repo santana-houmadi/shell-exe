cd3f835d1b3183e6b5736197041ac2a9b5c75a87
santana-houmadi <santana.houmadi@laplateforme.io>
2023-10-03 22:54:01 +0200

    ajout du job 03

---
+++ job03/add.sh
@@ -0,0 +1,9 @@
+#JOB 03
+echo "entrer le premier nombre"
+read num2
+echo "entrer le deuxième nombre"
+read num40
+resultat=$(($num2+$num40))
+echo "la somme de $num2 et $num40 est égale à $resultat"
+
+
